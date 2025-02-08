ee35d2d5a02b9686c914a03b2c0d26720d4a9f48
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2025-02-08 17:53:55 +0900

    Update BIRM.py

--- trainer/BIRM.py
+++ trainer/BIRM.py
@@ -63,7 +63,7 @@ def train(
             train_penalty = 0
             for i in range(sampleN):
                 ebd.re_init_with_noise(args.prior_sd_coef/args.data_num)
-                train_logits_w = ebd(env_num).view(-1, 1)*logits
+                train_logits_w = ebd(torch.tensor(env_num)).view(-1, 1)*logits
                 train_nll_env = criterion(train_logits_w, labels)
                 grad = autograd.grad(
                     train_nll_env * len(args.training_env), ebd.parameters(),
