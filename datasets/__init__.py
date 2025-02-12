ba0a814acbf52dfe57d60d759cdb73584a4090d9
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2025-02-12 14:05:30 +0900

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,4 +1,6 @@
 from datasets.colored_mnist import CMNIST
 from datasets.colored_fmnist import CFMNIST
+from datasets.colored_object import COCOcolor_LYPD
+
 
 __all__ = ["CMNIST", "CFMNIST", "COCOcolor_LYPD"]
