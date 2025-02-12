53bacabe098cd3e6ee62b7e4438718ee9b864d9a
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2025-02-12 13:59:39 +0900

    add

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from models.MLP import MLP180, MLP390, MLP540, MLP720, MLP_Gray, MLP_Extend, MLP1k, MLP2k, MLP360
+from models.ResNet import resnet18_sepfc_us
 
 __all__ = ["MLP180", "MLP360", "MLP390", "MLP540", "MLP720", "MLP1k", "MLP2k", "MLP_Gray", "MLP_Extend"]
 
