b866d75678a78c67b03587ae78f3697103228df2
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2025-03-11 21:33:53 +0900

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,7 @@
 from datasets.colored_mnist import CMNIST
 from datasets.colored_fmnist import CFMNIST
 from datasets.colored_object import COCOcolor_LYPD
+from datasets.pacs import PACS_FROM_DOMAINBED
 
 
-__all__ = ["CMNIST", "CFMNIST", "COCOcolor_LYPD"]
+__all__ = ["CMNIST", "CFMNIST", "COCOcolor_LYPD", "PACS_FROM_DOMAINBED"]
