f4306e5c236c2c86eac532bf63c0ef3c2cf3c2f4
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2025-03-11 22:59:29 +0900

    add

--- models/EBD.py
+++ models/EBD.py
@@ -7,26 +7,26 @@ class EBD(nn.Module):
     def __init__(self, flags):
       super(EBD, self).__init__()
       self.envs_num = len(flags.training_env)
-      # if self.flags.num_classes == 2:
+      if self.flags.num_classes == 2:
       self.embedings = torch.nn.Embedding(self.envs_num, 1)
-      # else:
-      #     self.embedings = torch.nn.Embedding(flags.envs_num, self.flags.num_classes)
+      else:
+          self.embedings = torch.nn.Embedding(flags.envs_num, self.flags.num_classes)
       self.re_init()
 
     def re_init(self):
       self.embedings.weight.data.fill_(1.)
 
     def re_init_with_noise(self, noise_sd):
-      # if self.flags.num_classes == 2:
-      rd = torch.normal(
-         torch.Tensor([1.0] * self.envs_num),
-         torch.Tensor([noise_sd] * self.envs_num))
-      self.embedings.weight.data = rd.view(-1, 1).cuda()
-      # else:
-      # rd = torch.normal(
-      #    torch.Tensor([1.0] * self.flags.envs_num * self.flags.num_classes),
-      #    torch.Tensor([noise_sd] * self.flags.envs_num* self.flags.num_classes))
-      # self.embedings.weight.data = rd.view(-1, self.flags.num_classes).cuda()
+      if self.flags.num_classes == 2:
+        rd = torch.normal(
+          torch.Tensor([1.0] * self.envs_num),
+          torch.Tensor([noise_sd] * self.envs_num))
+        self.embedings.weight.data = rd.view(-1, 1).cuda()
+      else:
+        rd = torch.normal(
+          torch.Tensor([1.0] * self.flags.envs_num * self.flags.num_classes),
+          torch.Tensor([noise_sd] * self.flags.envs_num* self.flags.num_classes))
+        self.embedings.weight.data = rd.view(-1, self.flags.num_classes).cuda()
 
     def forward(self, e):
       return self.embedings(e.long())
