5226736cb0cab9bfde2e3479f0320f195f005d69
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2025-05-30 12:50:44 +0900

    add

--- exp/T4/domainnet/vbirm.sh
+++ exp/T4/domainnet/vbirm.sh
@@ -1,40 +1,40 @@
-DATASET="PACS_FROM_DOMAINBED"
-EPOCHS=200
+DATASET="DomainNet_FROM_DOMAINBED"
+ARCH="resnet50"
+EPOCHS=10
 TRAIN_BATCH_SIZE=32
 SEED=2020
 TRAINER="vBIRM"
 DATA_DIR="/gs/bs/tge-24IJ0078/dataset"
 OPTIM="adam"
-TRAINING_ENV="0 1 3"
+TRAINING_ENV="1 2 3 4 5"
 TRAINING_CLASS_ENV=""
 TRAINING_COLOR_ENV=""
-TEST_ENV=2
+TEST_ENV=0
 LABEL_FLIP_P=0.25
-WD=0.00110794568
-PENALTY_WEIGHT=1
-LR=5e-4
-WARM_START=50
-OMEGA_LR=0.1
+WD=0.0
+PENALTY_WEIGHT=0
+LR=5e-5
+WARM_START=0
+OMEGA_LR=0.002
 PRINT_FREQ=100
 RESULT_DIR="./results"
 GPU="0"
 NO_CUDA=""
-WANDB_PROJECT_NAME="vBIRM_PACS"
-NUM_CLASSES=7
+WANDB_PROJECT_NAME="VBIRM_DOMAINNET"
+NUM_CLASSES=345
+SEED=2021
 
 PRIOR_SD_COEF=1000
 DATA_NUM=8321
 
-SEEDS=(2021 2022)
-VAR_BETAS=(1e-1 2e-1 3e-1 4e-1 5e-1 6e-1 7e-1 8e-1 9e-1)
-var_beta=0.5
+LAMBDA=(1e-3 1e-2 1e-1 1e0 1e1)
 count=0
 
-for seed in "${SEEDS[@]}" ; do
+for lambda in "${LAMBDA[@]}" ; do
     SHELL_ARGS="--dataset ${DATASET} \
                 --epochs ${EPOCHS} \
                 --train_batch_size ${TRAIN_BATCH_SIZE} \
-                --seed ${seed} \
+                --seed ${SEED} \
                 --trainer ${TRAINER} \
                 --data_dir ${DATA_DIR} \
                 --optim ${OPTIM} \
@@ -42,19 +42,18 @@ for seed in "${SEEDS[@]}" ; do
                 --training_env ${TRAINING_ENV} \
                 --test_env ${TEST_ENV} \
                 --wd ${WD} \
-                --penalty_weight ${PENALTY_WEIGHT} \
+                --penalty_weight ${lambda} \
                 --lr ${LR} \
                 --warm_start ${WARM_START} \
                 --omega_lr ${OMEGA_LR} \
                 --print_freq ${PRINT_FREQ} \
                 --wandb_project_name ${WANDB_PROJECT_NAME} \
-                --prior_sd_coef ${PRIOR_SD_COEF} \
-                --data_num ${DATA_NUM} \
+                --arch "resnet50" \
                 --num_classes ${NUM_CLASSES} \
-                --var_beta ${var_beta} \
-                "
+                --prior_sd_coef ${PRIOR_SD_COEF} \
+                --data_num ${DATA_NUM}"
 
-    CMD="qsub -g tge-24IJ0078 run.sh ${SHELL_ARGS}"
+    CMD="qsub -g tga-SlavakisLab run.sh ${SHELL_ARGS}"
     echo "Exp-$((count + 1)): ${CMD}"
     eval $CMD
 
